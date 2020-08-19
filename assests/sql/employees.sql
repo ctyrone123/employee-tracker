375829c9ecbb65c154dd50a712e3d4447b4410a5
Ctyrone123 <Ctyrone123@gmail.com>
2020-08-19 09:25:18 -0500

    filled db

---
+++ assests/sql/employees.sql
@@ -0,0 +1,57 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+use employees;
+INSERT INTO department
+    (name)
+VALUES
+    ('Operations'),
+    ('Analytics'),
+    ('Marketing'),
+    ('Executive');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('General Manager', 11000000, 1),
+    ('Coach', 4000000, 1),
+    ('Team Lead Analyst', 15000000, 2),
+    ('Team Analyst', 8000000, 2),
+    ('Media Manager', 7000000, 3),
+    ('Media Specialist', 3000000, 3),
+    ('CEO', 45000000, 4),
+    ('CEO Assistant', 25000000, 4);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Bob', 'Ross', 1, NULL),
+    ('Paul', 'Walker', 2, 1),
+    ('Chris', 'Tyrone', 3, NULL),
+    ('Vin', 'Diesel', 4, 3),
+    ('Dwayne', 'Johnson', 5, NULL),
+    ('Clark', 'Kent', 6, 5),
+    ('Bruce', 'Wayne', 7, NULL),
+    ('Tony', 'Stark', 8, 7);
+    
